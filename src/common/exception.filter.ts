6446c506a7d363a049110ad64f9162e6834f6199
Thomas Luu <52102141+dunglucbac@users.noreply.github.com>
2026-05-19 14:31:46 +0700

    Refactor error message handling in GlobalExceptionFilter
    - Improved type safety by explicitly defining the response structure for HttpException.
    - Enhanced readability by restructuring the error message extraction logic.
    - Streamlined the JSON response formatting for error handling in the global exception filter.

--- src/common/exception.filter.ts
+++ src/common/exception.filter.ts
@@ -20,15 +20,16 @@ export class GlobalExceptionFilter implements ExceptionFilter {
 
     const message =
       exception instanceof HttpException
-        ? (exception.getResponse() as any).message ?? exception.message
+        ? ((exception.getResponse() as { message: string }).message ??
+          exception.message)
         : 'Internal server error';
 
-    const errorMessage = Array.isArray(message) ? message[0] : message;
+    const errorMessage = Array.isArray(message)
+      ? (message[0] as string)
+      : message;
 
-    response.status(status).json({
-      status: 'error',
-      message: errorMessage,
-      data: null,
-    });
+    response
+      .status(status)
+      .json({ status: 'error', message: errorMessage, data: null });
   }
 }
